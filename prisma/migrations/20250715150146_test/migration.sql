fcd7361a1756cb58e625c3b97bea3e7132a0e2c3
jorgev259 <jorgev259@outlook.es>
2025-07-15 09:07:35 -0600

    Implement Prisma ORM client

---
+++ prisma/migrations/20250715150146_test/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Generic` (
+    `id` VARCHAR(191) NOT NULL,
+    `col1` VARCHAR(191) NOT NULL,
+    `col2` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
